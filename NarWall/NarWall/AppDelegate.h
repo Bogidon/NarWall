3acdaefe677209ad43a127bd6f5f45c228c2f399
Bogdan Vitoc <bogdanvitoc@gmail.com>
2014-11-14 22:33:28 -0500

    Added Xcode project

---
+++ NarWall/NarWall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NarWall
+//
+//  Created by Bogdan Vitoc on 11/14/14.
+//  Copyright (c) 2014 New School Crew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NarWall/NarWall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NarWall
+//
+//  Created by Bogdan Vitoc on 11/14/14.
+//  Copyright (c) 2014 New School Crew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
