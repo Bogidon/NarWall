b99794de983e0c99b7b7db1e5fb125c99c42c367
Michael Hulet <bestraysfan@me.com>
2014-11-15 00:33:53 -0500

    Added 2 new custom fonts

--- NarWall/NarWall/AppDelegate.h
+++ NarWall/NarWall/AppDelegate.h
@@ -10,3 +10,5 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 @property (strong, nonatomic) UIWindow *window;
 @end
+
+//Global font names: ITCFranklinGothicStd-Med, ITC Franklin Gothic Std Heavy
