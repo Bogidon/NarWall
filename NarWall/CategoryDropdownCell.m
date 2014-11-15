a2ac222cf79d4dae338025b0e18e8bbd4dbfa8a6
Bogdan Vitoc <bogdanvitoc@gmail.com>
2014-11-15 12:31:31 -0500

    Failed at chevron

--- NarWall/CategoryDropdownCell.m
+++ NarWall/CategoryDropdownCell.m
@@ -20,29 +20,29 @@
 @implementation CategoryDropdownCell
 
 -(void)willMoveToSuperview:(UIView *)newSuperview {
-    self.disclosureView.transform = CGAffineTransformMakeRotation(M_PI_2 * 3);
     
     [self spinWithOptions:UIViewAnimationOptionCurveEaseInOut];
 }
 
 - (void) spinWithOptions: (UIViewAnimationOptions) options {
-    // this spin completes 360 degrees every 2 seconds
-    [UIView animateWithDuration: 2.5f
-                          delay: 0.0f
+    
+    [UIView animateWithDuration: 0.5f
+                          delay: 2.0f
                         options: options
                      animations: ^{
-                         self.disclosureView.transform = CGAffineTransformRotate(self.disclosureView.transform, M_PI);
+                         self.disclosureView.transform = CGAffineTransformMakeRotation(M_PI);
+//                         self.disclosureView.transform = CGAffineTransformRotate(self.disclosureView.transform, M_PI);
                      }
                      completion: ^(BOOL finished) {
-                         if (finished) {
-                             if (animating) {
-                                 // if flag still set, keep spinning with constant speed
-                                 [self spinWithOptions: UIViewAnimationOptionCurveLinear];
-                             } else if (options != UIViewAnimationOptionCurveEaseOut) {
-                                 // one last spin, with deceleration
-                                 [self spinWithOptions: UIViewAnimationOptionCurveEaseOut];
-                             }
-                         }
+//                         if (finished) {
+//                             if (animating) {
+//                                 // if flag still set, keep spinning with constant speed
+//                                 [self spinWithOptions: UIViewAnimationOptionCurveLinear];
+//                             } else if (options != UIViewAnimationOptionCurveEaseOut) {
+//                                 // one last spin, with deceleration
+//                                 [self spinWithOptions: UIViewAnimationOptionCurveEaseOut];
+//                             }
+//                         }
                      }];
 }
 
